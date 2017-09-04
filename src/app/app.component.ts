0dcff33bca5a6e3d5f67055ad27846ebb0212b85
Bert J Zylstra <bert.zylstra65@gmail.com>
2017-09-04 14:49:12 -0600

    Display list of camps from mock data
    - function to display date range
    - declare mock data
    - add some style stuff from the tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,38 @@
 import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'Camp Assignments';
-  currentCamp: Camp = {
-    id: 1,
-    description: 'Junior 2',
-    startDate: new Date('July 7, 2017'),
-    endDate: new Date('July 12, 2017')
-  };
-}
-
 export class Camp {
   id: number;
   description: string;
   startDate: Date;
   endDate: Date;
+  constructor(id: number, description: string, startDate: Date, endDate: Date) {
+    this.id = id;
+    this.description = description;
+    this.startDate = startDate;
+    this.endDate = endDate;
+  }
+  dateRangeText(): string {
+    const options = {year: 'numeric', month: 'long', day: 'numeric'};
+    return this.startDate.toLocaleDateString('EN-US', options) + ' - ' +
+      this.endDate.toLocaleDateString('EN-US', options);
+  }
 }
 
+const CAMPS: Camp[] = [
+  new Camp(1, 'Junior 2', new Date('July 7, 2017'), new Date('July 12, 2017')),
+  new Camp(2, 'Junior 1', new Date('July 1, 2017'), new Date('July 5, 2017'))
+];
 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Camp Assignments';
+  currentCampId = 1;
+  camps = CAMPS;
+  currentCamp: Camp = CAMPS[1];
+}
